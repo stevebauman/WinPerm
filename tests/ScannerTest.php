dea1b6c6290cea15ecca73397e2dfe1f05aa0626
Steve Bauman <steven_bauman@outlook.com>
2017-10-21 11:29:33 -0400

    Reworked tests

--- tests/ScannerTest.php
+++ tests/ScannerTest.php
@@ -2,22 +2,22 @@
 
 namespace Stevebauman\WinPerm\Tests;
 
-use Stevebauman\WinPerm\Permission;
+use Stevebauman\WinPerm\Scanner;
 
-class WinPermTestCase extends \PHPUnit_Framework_TestCase
+class ScannerTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
     public function can_be_constructed_without_path()
     {
-        $permission = new Permission();
+        $permission = new Scanner();
 
-        $this->assertInstanceOf(Permission::class, $permission);
+        $this->assertInstanceOf(Scanner::class, $permission);
     }
 
     /** @test */
     public function valid_path_does_not_throw_exception()
     {
-        new Permission(__DIR__);
+        new Scanner(__DIR__);
 
         $passes = true;
 
@@ -30,6 +30,6 @@ class WinPermTestCase extends \PHPUnit_Framework_TestCase
      */
     public function invalid_path_throws_exception()
     {
-        new Permission('invalid path');
+        new Scanner('invalid path');
     }
 }
