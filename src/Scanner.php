8f38c112736f8c92825e3e7f4c0ea3f7bfd72136
Steve Bauman <steven_bauman@outlook.com>
2017-10-21 11:29:26 -0400

    Renamed Permission class to Scanner

---
+++ src/Scanner.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Stevebauman\WinPerm;
+
+use Stevebauman\WinPerm\Exceptions\InvalidPathException;
+
+class Scanner
+{
+    /**
+     * The command to use to check for permissions.
+     *
+     * @var string
+     */
+    protected $command = 'icacls';
+
+    /**
+     * The path to check.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Constructor.
+     *
+     * @param string $path The path to check permissions on.
+     *
+     * @throws InvalidPathException
+     */
+    public function __construct($path = null)
+    {
+        if (!is_null($path)) {
+            $this->setPath($path);
+        }
+    }
+
+    /**
+     * Sets the path to check permissions on.
+     *
+     * @param string $path
+     *
+     * @throws InvalidPathException
+     */
+    public function setPath($path)
+    {
+        if (file_exists($path)) {
+            $this->path = $path;
+        } else {
+            throw new InvalidPathException('File or Folder does not exist.');
+        }
+    }
+
+    /**
+     * Executes and processes account permissions for the current path.
+     *
+     * @return array|bool
+     */
+    public function check()
+    {
+        $command = sprintf('%s "%s"', $this->command, addslashes($this->path));
+
+        exec($command, $output, $return);
+
+        if ($return === 0 && is_array($output)) {
+            // The first element will always include the path, we'll
+            // remove it before passing the output to the parser.
+            $output[0] = str_replace($this->path, '', $output[0]);
+
+            // The last element will always contain the success / failure message.
+            array_pop($output);
+
+            // We'll also remove blank lines from the output array.
+            $output = array_filter($output);
+
+            // Finally, we'll pass the output to the parser.
+            return $this->newParser($output)->parse();
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns a new parser instance.
+     *
+     * @param array $output
+     *
+     * @return Parser
+     */
+    protected function newParser(array $output = [])
+    {
+        return new Parser($output);
+    }
+}
